18043f45d7fbbab0f86813cb354f6f68cb5e4c37
PrajwalKoirala <49274978+PrajwalKoirala@users.noreply.github.com>
2020-07-25 17:27:03 +0545

    Update lqr_cartpole.m

--- lqr_cartpole.m
+++ lqr_cartpole.m
@@ -1,8 +1,16 @@
-g = 9.8; M_c = 1; m_p = 0.1; l = 0.5;
-A = [0 1 0 0; 0 0 -3*m_p*g/(m_p+4*M_c) 0; 0 0 0 1; 0 0 3*g*(m_p+M_c)/((m_p+4*M_c)*l) 0]
-B = [0 4/(m_p+4*M_c) 0 -3/(m_p+4*M_c)]'
+syms x dx theta dtheta u;     % define symbolic variables x, dx, theta, dtheta and u
+accelerations = calc_acc(x, dx, theta, dtheta, u);     % calculate angular and translational accelerations
+f = [dx; accelerations(1); dtheta; accelerations(2)];  % f is our nonlinear function of state variables and u
 
+% calculates the partial derivatives :
+A = jacobian(f, [x; dx; theta; dtheta]);         
+B = jacobian(f, u);
 
+% linearize about [x, theta, dx, dtheta, u] = [0,0,0,0, 0]
+A = double(subs(A, {x, theta, dx, dtheta, u},{0,0,0,0, 0}))
+B = double(subs(B, {x, theta, dx, dtheta, u},{0,0,0,0, 0}))
+
+% Q and R values give corresponding weightage to variables and control signal
 Q = [1  0   0   0;
      0  10  0   0;
      0  0   1   0;
